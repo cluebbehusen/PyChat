c8008b1f76c7d2aad64cb28d320cb93a6e2f1f6e
Connor Luebbehusen <connorluebbehusen@hotmail.com>
2019-07-30 19:32:34 -0500

    added initial multi client support and basic functionality

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
+# System libraries
 import argparse
+import select
+import sys
 
+# Local libraries
 from utils import *
 
 # Parse command line arguments
@@ -9,3 +13,31 @@ parser.add_argument('-i', metavar='IP', dest='ip_addr', required=True,
 args = parser.parse_args()
 ip_address = args.ip_addr
 port = SERVER_PORT
+
+try:
+    server_connection_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_connection_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_connection_socket.connect((ip_address, port))
+except Exception as e:
+    print('[!] Error: Failed to set up server connection')
+    print(e)
+    sys.exit(1)
+
+print('Successfully connected to server.')
+print('Type messages at any point')
+
+connection_list = []
+
+connection_list.append(sys.stdin)
+connection_list.append(server_connection_socket)
+
+while True:
+    ready_connections, w_l, x_l = select.select(connection_list, [], [])
+    for connection in ready_connections:
+        if connection is server_connection_socket:
+            message = server_connection_socket.recv(1024)
+            print(message.decode())
+        else:
+            pass
+            message = input('Enter your message: ')
+            server_connection_socket.sendall(message.encode())
--- server.py
+++ server.py
@@ -16,22 +16,25 @@ ip_address = args.ip_addr
 port = SERVER_PORT
 
 # Initialize a list to hold all client connections
-listen_list = []
+connection_list = []
 
 # Setup the main server socket
 server_socket = create_socket(ip_address, port)
-listen_list.append(server_socket)
+connection_list.append(server_socket)
 
 while True:
     # Use the select library to only listen to sockets that are ready to have
     #     data read from them
-    client_list, w_l, x_l = select.select(listen_list, [], [])
+    client_list, w_l, x_l = select.select(connection_list, [], [])
     for client in client_list:
         # Check if the connection is new
         if client is server_socket:
             connection, address = client.accept()
             new_client = Client(connection)
-            listen_list.append(new_client)
+            connection_list.append(new_client)
         # If the connection is not new, data has been received, handle it
         else:
             message = client.socket.recv(1024)
+            for connection in connection_list:
+                if connection is not server_socket:
+                    connection.socket.sendall(message)
